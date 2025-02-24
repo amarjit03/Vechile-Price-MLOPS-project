e1cf2898eaa9e1ef7d9043044800c7b8b9223969
Amarjit Singh <118879833+amarjit03@users.noreply.github.com>
2025-02-24 14:23:45 +0530

    added exception handling file

--- demo.py
+++ demo.py
@@ -1,7 +1,18 @@
-from src.logger import logging
+# from src.logger import logging
 
-logging.debug("This is a debug message")
-logging.info("This is an info message")
-logging.warning("This is a warning message")
-logging.error("This is an error message")
-logging.critical("This is a critical message")
+# logging.debug("This is a debug message")
+# logging.info("This is an info message")
+# logging.warning("This is a warning message")
+# logging.error("This is an error message")
+# logging.critical("This is a critical message")
+
+
+import sys
+from src.logger import logging
+from src.exception import MyException
+
+try:
+    a = 1+'z' 
+except Exception as e:
+    logging.info(e)
+    raise MyException(e,sys) from e
